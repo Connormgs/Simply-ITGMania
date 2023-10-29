676909ea282f35a2f7382964bcb865de401a2f51
Connormgs <connormgs@gmail.com>
2023-10-29 00:36:14 -0500

    a
    a

--- Graphics/MusicWheelItem Song NormalPart.lua
+++ Graphics/MusicWheelItem Song NormalPart.lua
@@ -36,7 +36,7 @@ return Def.ActorFrame{
 			Font="_eurostile normal",
 			Name="Title",
 			InitCommand=function(self)
-				self:x(95):maxwidth(350):halign(1):shadowlength(1)
+				self:x(95):maxwidth(450):halign(1):shadowlength(1)
 			end
 		},
 	
