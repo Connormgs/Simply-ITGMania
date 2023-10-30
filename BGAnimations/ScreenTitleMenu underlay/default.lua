a5e4a1491221d58fe598c781690fa0fb81c9ddd4
Connormgs <connormgs@gmail.com>
2023-10-30 06:31:57 -0500

    Test
     test

--- BGAnimations/ScreenTitleMenu underlay/default.lua
+++ BGAnimations/ScreenTitleMenu underlay/default.lua
@@ -1,5 +1,6 @@
 local style = ThemePrefs.Get("ITG1") and "_flare" or "flare"
 local num = ThemePrefs.Get("ITG1") and "" or " 2"
+InitializeSimplyLove()
 return Def.ActorFrame{
 	LoadActor("../ScreenLogo background"),
 
