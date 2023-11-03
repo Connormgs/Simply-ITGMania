cd3669fa81241a0cb8f766a73dbc9c7f9e04a348
Connormgs <connormgs@gmail.com>
2023-11-02 22:12:51 -0500

    sound fixes
     sound fixes

--- BGAnimations/ScreenSelectMusic overlay/overlay.lua
+++ BGAnimations/ScreenSelectMusic overlay/overlay.lua
@@ -214,12 +214,7 @@ Def.BitmapText{
 			SelectMenuClosedMessageCommand=function(self)
 				self:stoptweening():linear(0.2):diffusealpha(0):zoomx(0.5)
 			end,
-			ShowPressStartForOptionsCommand=function(self)
-				SOUND:PlayOnce( ThemePrefs.Get("ITG1") and THEME:GetPathS("ITG1/Common","start") or THEME:GetPathS("_ITGCommon","start") )
-			end,
-			ShowEnteringOptionsCommand=function(self)
-				SOUND:PlayOnce( ThemePrefs.Get("ITG1") and THEME:GetPathS("ITG1/Common","start") or THEME:GetPathS("_ITGCommon","start") )
-			end
+
 		}
 	},
 
@@ -250,11 +245,11 @@ Def.Sprite{
 		end,
 		ShowPressStartForOptionsCommand=function(self)
 			self:diffusealpha(1):faderight(.3):fadeleft(.3):cropleft(-0.3):cropright(1.3):linear(0.3):cropright(-0.3):sleep(1.2):linear(0.3):cropleft(1.3)
-			SOUND:PlayOnce( THEME:GetPathS("_ITGCommon","start") )
+			
 		end,
 		ShowEnteringOptionsCommand=function(self)
 			self:stoptweening():setstate(1):sleep(0.6):linear(0.3):cropleft(1.3)
-			SOUND:PlayOnce( THEME:GetPathS("_ITGCommon","start") )
+			
 		end,
 		HidePressStartForOptionsCommandCommand=function(self)
 			self:linear(0.3):cropleft(1.3)
